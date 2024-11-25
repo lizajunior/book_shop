db3ebb23ebe27080abb046adffdbe1853adc099b
liza <lizadzundiet@gmail.com>
2024-11-26 00:22:44 +0200

    problems  with sass, css and js

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,20 @@ module.exports = {
           'sass-loader',  // Компилирует Sass в CSS
         ],
       },
+
+      {
+        test: /\.js$/, // Обработка .js файлов
+        exclude: /node_modules/,
+        use: 'babel-loader', // Если нужен Babel для транспиляции
+      },
+      
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
     ],
   },
   plugins: [
@@ -24,5 +38,11 @@ module.exports = {
       filename: 'styles.css', // Имя для итогового CSS-файла
     }),
   ],
+
+  output: {
+    filename: 'bundle.js', // Имя итогового JS-файла
+    path: __dirname + '/dist', // Путь для сборки
+  },
+
   mode: 'development', // Для production используйте 'production'
 };
